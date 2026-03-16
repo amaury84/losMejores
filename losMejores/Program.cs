086bec02bf35da02e62a75d5ba1104b5a0001db1
Amaury <amaury84@gmail.com>
2026-03-16 00:44:57 -0500

    mejorando el codigo

--- losMejores/Program.cs
+++ losMejores/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(20*200);
+﻿using System;
+
+class Program{
+    static void Main(){
+        Console.WriteLine("Nuevo");
+    }
+}
