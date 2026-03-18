f106d1dd4a3f4a7cb0030c9671bcec90fb0696b9
amaury84 <72026866+amaury84@users.noreply.github.com>
2026-03-17 21:32:07 -0500

    Add comment in Program.cs

--- losMejores/Program.cs
+++ losMejores/Program.cs
@@ -3,6 +3,7 @@
 class Program{
     static void Main(){
         Console.WriteLine("Nuevo");
+        //nuevo comentario
         Contador contador = new Contador();
         var cuenta= contador.Incrementar();
         Console.WriteLine(cuenta);
